249d88d8832ea28aaac75fd05c7bc0a1b7c9425b
Priya <priyajha7585@gmail.com>
2022-11-13 12:39:48 +0530

    Worked on responsiveness of  header and carousel

--- src/CarouselSection.js
+++ src/CarouselSection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import img1 from './assets/anime-night-neon-191.jpg';
 import img2 from './assets/digital-digital-art-artwork-night-city-hd-wallpaper-preview.jpg';
 import img3 from './assets/neon_lights_city_cyberpunk_buildings_4k_hd_neon.jpg';
@@ -25,7 +25,16 @@ const images = [
 ]
 
 function CarouselSection() {
-    const [imgid, setImgid] = React.useState(1);
+  const [width, setWidth] = useState(window.innerWidth);
+  const [imgid, setImgid] = React.useState(1);
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
     function makeTimer()
     {
@@ -45,16 +54,33 @@ function CarouselSection() {
   return (
     <>
     {makeTimer()}
-    <center>
-    <div style={{height:"60vh", width:"80vw", padding:"10vh 10vh 1vh 10vh"}}>
-    {images.map((image)=> image.id===imgid && <img src={image.image} style={{height:"100%", width:"100%", borderRadius:"50px", padding:"6px", background:"linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)"}}/>)}
-    </div></center>
-    <div style={{display:"flex", justifyContent:"center"}}>
-    {images.map((image)=><button style={{backgroundColor:"transparent"}} onClick={()=>setImgid(image.id)}><hr className={`${image.id===imgid ? "active" : "inactive"}`}/></button>)}
-    </div><br/><br/>
-    <h1 className='animate-character'>
-    Experience the new era of Entertainment
-    </h1>
+    {width>767 ?
+      <> 
+      <center>
+      <div style={{height:"60vh", width:"80vw", padding:"10vh 10vh 1vh 10vh"}}>
+      {images.map((image)=> image.id===imgid && <img src={image.image} style={{height:"100%", width:"100%", borderRadius:"50px", padding:"6px", background:"linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)"}}/>)}
+      </div></center>
+      <div style={{display:"flex", justifyContent:"center"}}>
+      {images.map((image)=><button style={{backgroundColor:"transparent", border:"none"}} onClick={()=>setImgid(image.id)}><hr className={`${image.id===imgid ? "active" : "inactive"}`}/></button>)}
+      </div><br/><br/>
+      <h1 className='animate-character'>
+      Experience the new era of Entertainment
+      </h1>
+      </>
+    :
+    <>
+      <center>
+      <div style={{height:"60vh", width:"80vw", padding:"10vh 1vh 1vh 1vh"}}>
+      {images.map((image)=> image.id===imgid && <img src={image.image} style={{height:"100%", width:"100%", borderRadius:"50px", padding:"6px", background:"linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)"}}/>)}
+      </div></center>
+      <div style={{display:"flex", justifyContent:"center"}}>
+      {images.map((image)=><button style={{backgroundColor:"transparent", border:"none"}} onClick={()=>setImgid(image.id)}><hr className={`${image.id===imgid ? "active" : "inactive"}`}/></button>)}
+      </div><br/><br/>
+      <h1 className='animate-character'>
+      Experience the new era of Entertainment
+      </h1>
+    </>
+    }
 
 {/*
 <div className="tag">
--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Header.css';
 import logo from './assets/channels4_profile1.jpg'
 
 function Header() {
+  const [width, setWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
   return (
-    <div className="header">
-      <div className='logo-image' style={{backgroundColor:"pink"}}>
+    <>
+    
+    {width>767 ?
+      <div className="header">
+      <div className='logo-image'>
       <img src={logo} />
-      <h2>HEFTYVERSE</h2>
+      <h2>&nbsp;HEFTYVERSE</h2>
       </div>
-      <div className='button1' style={{backgroundColor:"green"}}>
-      <button type="submit">
-      LogOut
-      </button>
+      <div className='button1'>
+      <h3>Welcome! Priya Jha</h3>
+      <button type="submit">LogOut</button>
       </div>
     </div>
+    :
+    <div className="header">
+      <div className='logo-image'>
+      <img src={logo} />
+      <h4>&nbsp;HEFTYVERSE</h4>
+      </div>
+      <div className='button2'>
+      <h5>Welcome! Priya Jha</h5>
+      <button type="submit" style={{margin:"5px 10px 5px 5px"}}>LogOut</button>
+      </div>
+    </div>
+    }</>
+    
   )
 }
 
